df71f0b6d2210d9c637fc1478ece97173a8166d0
lenayak <113600536+lenayak@users.noreply.github.com>
2022-11-14 01:51:57 +0400

    Registration according to PEP 8

--- .py
+++ .py
@@ -1,6 +1,6 @@
 import os
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup as bs
 from time import sleep
 
 
@@ -15,14 +15,18 @@ class Review:
             print("Некорректное содержание.")
             exit()
         self.review = review         
-        if mark >= 0 and mark <= 5:
+        if (mark >= 0) and (mark <= 5):
             self.mark = mark
+
     def get_mark(self):
         return self.mark
+
     def get_name(self):
         return self.name
+
     def get_review(self):
         return self.review
+
 
 headers = {           
     'Connection': 'keep-alive',
@@ -35,18 +39,22 @@ headers = {
     'Accept-Encoding': 'gzip, deflate, lzma, sdch',
     'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4'
 }
+
+
 def get_page(URL):        
     try:
         html_page = requests.get(URL, headers = headers)
         sleep(2)
         if html_page.status_code == 200:
-            return BeautifulSoup(html_page.content, 'lxml')
+            return bs(html_page.content, 'lxml')
         else:
             print("Страница не найдена.")
             exit()
     except:
         print('Ошибка соединения.')
         return -1
+
+
 def get_marks(articles):       
     marks = list()
     for article in articles:   
@@ -59,6 +67,8 @@ def get_marks(articles):
             print('Не найдена оценка.')
             return -1
     return marks
+
+
 def get_names(articles):          
     names = list()
     for article in articles:    
@@ -71,6 +81,8 @@ def get_names(articles):
             print('Не найдено название.')
             return -1
     return names
+
+
 def get_reviews(articles):            
     reviews = list()
     for article in articles:
@@ -83,6 +95,8 @@ def get_reviews(articles):
             print('Не найдена рецензия.')
             return -1
     return reviews
+
+
 def parse_pages(max_num_of_requests, least_num_of_marks):   
     dataset = list()
     one = 0
@@ -101,8 +115,8 @@ def parse_pages(max_num_of_requests, least_num_of_marks):
             continue
         try:
             articles = (             
-                soup.find('main', class_ = "main-body page-content")
-                .find('section', class_ = "lenta__content")
+                soup.find('main', class_="main-body page-content")
+                .find('section', class_="lenta__content")
                 .find_all("article", class_="review-card lenta__item")
             )
         except AttributeError as e:
@@ -121,31 +135,31 @@ def parse_pages(max_num_of_requests, least_num_of_marks):
             k = Review(names[j], reviews[j], float(marks[j]))  
             if k.mark < 1.5:              
                 if one < 1000:     
-                    one +=1
+                    one += 1
                     dataset.append(k)
                 else:           
                     one = one
-            elif (k.mark >= 1.5 and k.mark < 2.5):
+            elif (k.mark >= 1.5) and (k.mark < 2.5):
                 if two < 1000:
-                    two +=1
+                    two += 1
                     dataset.append(k)
                 else:
                     two = two
-            elif (k.mark >= 2.5 and k.mark < 3.5):
+            elif (k.mark >= 2.5) and (k.mark < 3.5):
                 if three < 1000:
-                    three +=1
+                    three += 1
                     dataset.append(k)
                 else: 
                     three = three
-            elif (k.mark >= 3.5 and k.mark < 4.5):
+            elif (k.mark >= 3.5) and (k.mark < 4.5):
                 if four < 1000:
-                    four +=1
+                    four += 1
                     dataset.append(k)
                 else:
                     four = four
-            elif (k.mark >= 4.5 and k.mark <= 5):
+            elif (k.mark >= 4.5) and (k.mark <= 5):
                 if five < 1000:
-                    five +=1
+                    five += 1
                     dataset.append(k)
                 else:
                     five = five          
@@ -161,18 +175,20 @@ def parse_pages(max_num_of_requests, least_num_of_marks):
             break
     return dataset   
 
+
 def create_folder():
     os.mkdir('dataset')
     for i in range(1,6):
         os.mkdir('dataset/'+str(i))
 
+
 def save_reviews(data, filename):
     for i in range(0, len(data)):
-         file = open(filename + f"\\{(i+1):04}" + ".txt", "w", encoding="utf-8")
-         file.write(data[i].get_name()) 
-         file.write("\n\n\n")
-         file.write(data[i].get_review())
-         file.close
+        file = open(filename + f"\\{(i+1):04}" + ".txt", "w", encoding="utf-8")
+        file.write(data[i].get_name()) 
+        file.write("\n\n\n")
+        file.write(data[i].get_review())
+        file.close
 
 
 if __name__ == "__main__":
@@ -181,14 +197,14 @@ if __name__ == "__main__":
     least_num_of_marks = 1000   
     dataset = parse_pages(max_num_of_requests, least_num_of_marks)
     create_folder()
-    one_data = [el for el in dataset if el.get_mark()<1.5]
+    one_data = [el for el in dataset if el.get_mark() < 1.5]
     save_reviews(one_data, "dataset\\1")
-    two_data = [el for el in dataset if el.get_mark()>=1.5 and el.get_mark()<2.5]
+    two_data = [el for el in dataset if (el.get_mark() >= 1.5) and (el.get_mark() < 2.5)]
     save_reviews(two_data, "dataset\\2")
-    three_data = [el for el in dataset if el.get_mark()>=2.5 and el.get_mark()<3.5]
+    three_data = [el for el in dataset if (el.get_mark() >= 2.5) and (el.get_mark() < 3.5)]
     save_reviews(three_data, "dataset\\3")
-    four_data = [el for el in dataset if el.get_mark()>=3.5 and el.get_mark()<4.5]
+    four_data = [el for el in dataset if (el.get_mark() >= 3.5) and (el.get_mark() < 4.5)]
     save_reviews(four_data, "dataset\\4")
-    five_data = [el for el in dataset if el.get_mark()>=4.5 and el.get_mark()<=5.0]
+    five_data = [el for el in dataset if (el.get_mark() >= 4.5) and (el.get_mark() <= 5.0)]
     save_reviews(five_data, "dataset\\5")
     print("Работа завершена.")
